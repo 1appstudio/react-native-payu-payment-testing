7446cf40a14ff6535defef23f7a67ec7888973de
appstudio@icloud.com <jk@JKs-MacBook-Pro.local>
2022-03-27 17:33:50 +0530

    1

--- ios/PayuPaymentTesting.h
+++
@@ -1,5 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface PayuPaymentTesting : NSObject <RCTBridgeModule>
-
-@end
---
+++ ios/PayuPaymentTesting.swift
@@ -0,0 +1,9 @@
+//
+//  PayuPaymentTesting.swift
+//  PayuPaymentTesting
+//
+//  Created by JK on 27/03/22.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+import Foundation
